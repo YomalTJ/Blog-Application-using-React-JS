a513e47d7f2ab41e8ed65938d9e1bc2d73077379
YomalTJ <yomalthushara2295266@gmail.com>
2024-06-17 13:08:48 +0530

    Blog Application

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import BlogDetails from './BlogDetails';
+import Create from './Create';
+import Home from './Home';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
